ec5d876468dfa8ae670f3e947395b04d1cdb85a2
reomatsuoka <reo199637458@gmail.com>
2021-02-03 00:13:29 +0900

    practice

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
